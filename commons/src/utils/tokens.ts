1773352d6c6477158445a9482f6382515be2d8eb
kevinand11 <kevinfizu@gmail.com>
2021-08-18 19:00:36 +0100

    feat: added exchangeOldForNewTokens function

--- commons/src/utils/tokens.ts
+++ commons/src/utils/tokens.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 import { accessTokenKey, refreshTokenKey } from '../config'
-import { AccessTokenExpired, NotAuthenticatedError } from '../errors'
+import { AccessTokenExpired, NotAuthenticatedError, RefreshTokenMisusedError } from '../errors'
 import { AuthUser, RefreshUser } from './authUser'
 import { getCacheInstance } from '../cache/'
 
@@ -23,8 +23,13 @@ export const makeRefreshToken = async (payload: RefreshUser) => {
 
 export const verifyAccessToken = async (token: string) => {
 	try {
-		return jwt.verify(token, accessTokenKey) as AuthUser
+		const user = jwt.verify(token, accessTokenKey) as AuthUser
+		const cachedToken = await getCachedAccessToken(user.id)
+		// Cached access token was deleted, eg by user roles being modified, so token needs to be treated as expired
+		if (token !== cachedToken) throw new AccessTokenExpired()
+		return user
 	} catch (err) {
+		if (err instanceof AccessTokenExpired) throw err
 		if (err instanceof jwt.TokenExpiredError) throw new AccessTokenExpired()
 		else throw new NotAuthenticatedError()
 	}
@@ -40,3 +45,43 @@ export const verifyRefreshToken = async (token: string) => {
 
 export const getCachedAccessToken = async (userId: string) => getCacheInstance.get(getAccessTokenKey(userId))
 export const getCachedRefreshToken = async (userId: string) => getCacheInstance.get(getRefreshTokenKey(userId))
+export const deleteCachedAccessToken = async (userId: string) => getCacheInstance.delete(getAccessTokenKey(userId))
+export const deleteCachedRefreshToken = async (userId: string) => getCacheInstance.delete(getRefreshTokenKey(userId))
+
+type Tokens = {
+	accessToken: string
+	refreshToken: string
+}
+
+export const exchangeOldForNewTokens = async (
+	{ accessToken, refreshToken }: Tokens,
+	getAuthUser: (id: string) => Promise<AuthUser>
+): Promise<Tokens> => {
+	let authUser = await verifyAccessToken(accessToken).catch((err) => {
+		if (err instanceof AccessTokenExpired) return null
+		else throw err
+	})
+
+	// If auth token is not expired, return it and the refresh token
+	if (authUser) return { accessToken, refreshToken }
+
+	const refreshUser = await verifyRefreshToken(refreshToken)
+	const cachedRefreshToken = await getCachedRefreshToken(refreshUser.id)
+
+	// If no cached value, means someone used your old token for a second time, so current one got deleted from cache
+	if (!cachedRefreshToken) throw new NotAuthenticatedError()
+
+	// If cached value is not equal, means someone is trying to use an old token for a second time
+	if (refreshToken !== cachedRefreshToken) {
+		await deleteCachedRefreshToken(refreshUser.id)
+		throw new RefreshTokenMisusedError()
+	}
+
+	// Use refresh id to call cb passed in to get the user auth details
+	authUser = await getAuthUser(refreshUser.id)
+
+	return {
+		accessToken: await makeAccessToken(authUser),
+		refreshToken: await makeRefreshToken(refreshUser),
+	}
+}
