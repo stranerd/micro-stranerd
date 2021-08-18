a206553927d96ad598a511be2d35e47f9f8d740f
kevinand11 <kevinfizu@gmail.com>
2021-08-19 00:43:03 +0100

    fix: nginx issue

--- example/src/services/mongoose.ts
+++ example/src/services/mongoose.ts
@@ -1,5 +1,7 @@
 import { getMongooseConnection } from '@utils/commons'
+import { logger } from '@utils/logger'
 
 const db = await getMongooseConnection()
+	.catch((err) => logger.error(err))
 
 export default db
