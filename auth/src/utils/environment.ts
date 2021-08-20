d3b4b6dd260871d9c4b05d8788369b97f0930c33
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-08-20 10:32:17 +0100

    fix: removed unused files

--- auth/src/utils/environment.ts
+++ auth/src/utils/environment.ts
@@ -1,3 +1,4 @@
 import { getEnvOrFail } from '@utils/commons'
 
-export const port = parseInt(getEnvOrFail('PORT') || '5000')
+export const port = parseInt(getEnvOrFail('PORT'))
+export const appId = getEnvOrFail('APP_ID')
