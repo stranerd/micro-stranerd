9e85c86ee34c2ca8274a3dff1fe33fb997fe0c8e
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-08-20 11:50:55 +0100

    fix: token handler switched to @stranerd/common

--- auth/src/application/use-cases/auth/authenticate-user.use-case.ts
+++ auth/src/application/use-cases/auth/authenticate-user.use-case.ts
@@ -1,26 +1,26 @@
 import { UseCase } from '../../base'
-import { AuthOutput, Credential, TokenInput } from '../../domain'
-import { GenerateAuthOutputUseCase } from './generate-auth-output.use-case'
+import { Credential, AuthOutput, TokenInput } from '../../domain'
+import { GenerateAuthOutputUseCase } from '../auth/generate-auth-output.use-case'
 import { IUserRepository } from '../../contracts/repository'
 
 export class AuthenticateUserUseCase implements UseCase<Credential, AuthOutput> {
-	repository
+    repository
 
-	constructor (repo: IUserRepository) {
-		this.repository = repo
-	}
+    constructor(repo: IUserRepository) {
+	     this.repository = repo
+    }
 
-	async execute (params: Credential): Promise<AuthOutput> {
-		const TokenPayload: TokenInput = await this.repository.authenticateUser(params)
-		if (TokenPayload) {
+    async execute(params: Credential): Promise<AuthOutput> {
+		 const TokenPayload: TokenInput = await this.repository.authenticateUser(params)
+		 if(TokenPayload) {
+            
+			 const response = new GenerateAuthOutputUseCase(this.repository).execute(TokenPayload)
 
-			const response = new GenerateAuthOutputUseCase(this.repository).execute(TokenPayload)
-
-			return new Promise((resolve) => resolve(response))
-
-		}
-
-		return new Promise((res, reject) => reject())
-	}
+			 return new Promise((resolve) => resolve(response))
+                
+			 }
+			 
+			 return new Promise((res, reject) => reject())
+    }
 
 }
