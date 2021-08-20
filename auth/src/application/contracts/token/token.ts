311168791a203e419ac3f730cd6f2d9953b69695
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-08-20 09:10:57 +0100

    fix: removed unused files

--- auth/src/application/contracts/token/token.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import { TokenInput,RefreshTokenModel, UserModel } from '../../domain'
-
-export interface ITokenHandler{
-    generateJwtToken(payload: TokenInput): Promise<string>;
-    randomTokenString(): string;
-    MakeRefreshTokenModel(token: String,userId: String | null): Promise<RefreshTokenModel>;
-}
