9739654c70926132cf8583b49851d92f2736c008
kevinand11 <kevinfizu@gmail.com>
2021-08-20 02:19:03 +0100

    chore: register emails service

--- emails/src/utils/environment.ts
+++ emails/src/utils/environment.ts
@@ -1,4 +1,4 @@
 import { getEnvOrFail } from '@utils/commons'
 
 export const port = parseInt(getEnvOrFail('PORT'))
-export const appId = parseInt(getEnvOrFail('APP_ID'))
+export const appId = getEnvOrFail('APP_ID')
