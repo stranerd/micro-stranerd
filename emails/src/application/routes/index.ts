26a942284247061b01119b7c063bc8e830865801
kevinand11 <kevinfizu@gmail.com>
2021-08-20 02:15:17 +0100

    feat: email service init

---
+++ emails/src/application/routes/index.ts
@@ -0,0 +1,17 @@
+import { makeController, Route, StatusCodes } from '@utils/commons'
+import { appId } from '@utils/environment'
+
+export const routes: Route[] = [
+	{
+		path: '/',
+		method: 'get',
+		controllers: [
+			makeController(async () => {
+				return {
+					status: StatusCodes.Ok,
+					result: `${ appId } service running`
+				}
+			})
+		]
+	}
+]
