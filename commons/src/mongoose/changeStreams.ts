b022586e66ad356f31d239bd8d504626c85d173d
kevinand11 <kevinfizu@gmail.com>
2021-09-01 00:41:42 +0100

    v0.0.1-alpha-65 install

--- commons/src/mongoose/changeStreams.ts
+++ commons/src/mongoose/changeStreams.ts
@@ -1,66 +1,35 @@
 import { mongoose } from './index'
 
-declare type DeepPartial<T> = T extends Nullable ? T : {
-	[K in keyof T]?: DeepPartial<T[K]>
-}
-
-type Require = string | number | boolean
-type Nullable = Require | undefined | null
-
 export type ChangeStreamCallbacks<Model> = {
-	created?: (data: Model) => Promise<void>
-	updated?: (data: Model, fields: DeepPartial<Model>) => Promise<void>
-	deleted?: (data: { _id: string }) => Promise<void>
+	created?: (data: { before: null, after: Model }) => Promise<void>
+	updated?: (data: { before: Model, after: Model }) => Promise<void>
+	deleted?: (data: { before: Model, after: null }) => Promise<void>
 }
 
 export async function generateChangeStreams<Model> (collection: mongoose.Model<Model | any>, callbacks: ChangeStreamCallbacks<Model>) {
-	if (callbacks.created) {
-		const createdPipeline = [{ $match: { operationType: 'insert' } }]
-		collection.watch(createdPipeline, { fullDocument: 'updateLookup' })
-			.on('change', async (data) => {
-				if (data.operationType === 'insert') await callbacks.created?.(data.fullDocument)
-			})
-	}
+	const cloneName = collection.collection.collectionName + '_streams_cloned'
+	const getClone = () => collection.collection.conn.db.collection(cloneName)
 
-	if (callbacks.deleted) {
-		const deletedPipeline = [{ $match: { operationType: 'delete' } }]
-		collection.watch(deletedPipeline, { fullDocument: 'updateLookup' })
-			.on('change', async (data) => {
-				if (data.operationType === 'delete') await callbacks.deleted?.({
-					_id: (data.documentKey._id as string).toString()
-				})
-			})
-	}
+	collection.watch([], { fullDocument: 'updateLookup' })
+		.on('change', async (data) => {
+			if (data.operationType === 'insert') {
+				const _id = data.documentKey._id
+				const after = data.fullDocument as Model
+				await getClone().insertOne({ ...after, _id })
+				await callbacks.created?.({ before: null, after })
+			}
 
-	if (callbacks.updated) {
-		const updatedPipeline = [{ $match: { operationType: 'update' } }]
-		collection.watch(updatedPipeline, { fullDocument: 'updateLookup' })
-			.on('change', async (data) => {
-				if (data.operationType === 'update') {
-					const { updatedFields, removedFields } = data.updateDescription
-					const changed = removedFields
-						.map((f) => f.toString())
-						.concat(Object.keys(updatedFields))
-					const fields = getObjectsFromKeys(changed)
-					await callbacks.updated?.(data.fullDocument, fields)
-				}
-			})
+			if (data.operationType === 'delete') {
+				const _id = data.documentKey._id
+				const before = await getClone().findOneAndDelete({ _id }) as Model
+				await callbacks.deleted?.({ before, after: null })
+			}
 
-	}
+			if (data.operationType === 'update' || data.operationType === 'replace') {
+				const _id = data.documentKey._id
+				const after = data.fullDocument as Model
+				const before = await getClone().findOneAndUpdate({ _id }, after) as Model
+				await callbacks.updated?.({ before: before!, after })
+			}
+		})
 }
-
-const deepMerge = (objFrom: any, objTo: any) => Object.keys(objFrom)
-	.reduce(
-		(merged, key) => {
-			merged[key] = objFrom[key] instanceof Object && !Array.isArray(objFrom[key])
-				? deepMerge(objFrom[key], merged[key] ?? {})
-				: objFrom[key]
-			return merged
-		}, { ...objTo }
-	)
-
-const formObject = (key: string) => key.split('.').reverse().reduce((acc, value) => {
-	return { [value]: acc ?? true }
-}, null as unknown as any)
-
-const getObjectsFromKeys = (keys: string[]) => keys.map(formObject).reduce(deepMerge, {})
