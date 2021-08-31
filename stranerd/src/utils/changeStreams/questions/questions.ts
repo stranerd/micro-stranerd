1f33615bfc8762fab6a081ecff751891ccd3d148
kevinand11 <kevinfizu@gmail.com>
2021-08-31 21:57:52 +0100

    v0.0.1-alpha-64 install

--- stranerd/src/utils/changeStreams/questions/questions.ts
+++ stranerd/src/utils/changeStreams/questions/questions.ts
@@ -4,18 +4,30 @@ import { UpdateTagsCount } from '@modules/questions'
 import { addUserCoins } from '@utils/modules/users/transactions'
 import { GetUsers } from '@modules/users'
 import { sendNotification } from '@utils/modules/users/notifications'
+import { getSocketEmitter } from '@index'
+import { QuestionMapper } from '@modules/questions/data/mappers'
 
 export const QuestionChangeStreamCallbacks: ChangeStreamCallbacks<QuestionFromModel> = {
-	created: async (question) => {
+	created: async (questionModel) => {
+		const question = new QuestionMapper().mapFrom(questionModel)!
+
+		// Broadcast new question for those connected to the channel
+		await getSocketEmitter().emitCreated('questions', question)
+
+		// Reduce user coins
 		await addUserCoins(question.userId, {
 			bronze: 0 - question.coins,
 			gold: 0
 		}, 'You paid coins to ask a question!')
+
+		// increment tags count
 		await UpdateTagsCount.execute({
 			tagIds: question.tags,
 			increment: true
 		})
+
 		// TODO: Increase user question's count
+
 		// Send notification to all tutors with strongest subject
 		const tutors = await GetUsers.execute({
 			where: [
@@ -25,7 +37,7 @@ export const QuestionChangeStreamCallbacks: ChangeStreamCallbacks<QuestionFromMo
 		tutors.results.map((t) => t.id).map((id) => {
 			sendNotification(id, {
 				body: 'A new question was just asked on Stranerd that you might be interested in. Go check it out',
-				action: `/questions/${ question._id }`
+				action: `/questions/${ question.id }`
 			}, 'New Question')
 		})
 	}
