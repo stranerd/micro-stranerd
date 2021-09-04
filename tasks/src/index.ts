379cb889c2471d208ce36e4b09a44d8b7b30b300
kevinand11 <kevinfizu@gmail.com>
2021-09-05 00:16:19 +0100

    feat: setup tasks service

---
+++ tasks/src/index.ts
@@ -0,0 +1,22 @@
+import { getNewServerInstance, Logger, setupMongooseConnection } from '@utils/commons'
+import { appId, port } from '@utils/environment'
+import { subscribers } from '@utils/events'
+import { routes } from '@application/routes'
+
+const app = getNewServerInstance(routes, { mine: [], admin: [], open: [] })
+export const getSocketEmitter = () => app.socketEmitter
+
+const start = async () => {
+	await setupMongooseConnection()
+	await app.start(port)
+	await Logger.info(`${ appId } api has started listening on port`, port)
+
+	await Promise.all(
+		Object.values(subscribers)
+			.map(async (subscriber) => {
+				await subscriber.subscribe()
+			})
+	)
+}
+
+start().then()
