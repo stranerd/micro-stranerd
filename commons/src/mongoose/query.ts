6ff2b72588478e056e35b10bea48fe0ae27f1498
kevinand11 <kevinfizu@gmail.com>
2021-09-06 20:41:17 +0100

    v0.0.1-alpha-79

--- commons/src/mongoose/query.ts
+++ commons/src/mongoose/query.ts
@@ -5,8 +5,11 @@ export enum Conditions {
 	'eq' = 'eq', 'ne' = 'ne', 'in' = 'in', 'nin' = 'nin'
 }
 
+type Where = { field: string, value: any, condition: Conditions }
+
 export type QueryParams = {
-	where?: { field: string, value: any, condition: Conditions }[]
+	where?: Where[]
+	auth?: { field: string, value: string }
 	whereType?: 'and' | 'or'
 	sort?: { field: string, order?: 1 | -1 }
 	limit?: number
@@ -16,29 +19,12 @@ export type QueryParams = {
 
 export async function parseQueryParams<Model> (collection: mongoose.Model<Model | any>, params: QueryParams): Promise<QueryResults<Model>> {
 	// Handle where clauses
-	const whereType = ['and', 'or'].indexOf(params.whereType as string) !== -1 ? params.whereType : 'and'
-	const where = (params.where ?? [])
-		.map(({ field, value, condition }) => {
-			const checkedField = field === 'id' ? '_id' : (field ?? '')
-			const parsedValue = value ?? ''
-			const checkedValue = field === 'id'
-				? mongoose.Types.ObjectId.isValid(parsedValue) ? parsedValue : new mongoose.Types.ObjectId()
-				: (parsedValue ?? '')
-			const checkedCondition = Object.keys(Conditions).indexOf(condition as unknown as string) > -1 ? condition : Conditions.eq
-			return ({
-				field: checkedField,
-				value: checkedValue,
-				condition: checkedCondition
-			})
-		})
-	const whereClause = {}
-	if (where.length > 0) {
-		whereClause[`$${ whereType }`] = where.map((c) => ({
-			[`${ c.field }`]: { [`$${ c.condition }`]: c.value }
-		}))
-	}
-
-	if (params.search) whereClause['$text'] = { $search: params.search }
+	const totalClause = { $and: [] as any }
+	const whereType = ['and', 'or'].indexOf(params.whereType as string) !== -1 ? params.whereType! : 'and'
+	const where = buildWhereQuery(params.where ?? [], whereType)
+	if (where) totalClause.$and.push(where)
+	if (params.auth) totalClause.$and.push({ [params.auth.field]: params.auth.value })
+	if (params.search) totalClause['$text'] = { $search: params.search }
 
 	// Handle sort clauses
 	const sortField = params.sort?.field ?? null
@@ -51,9 +37,9 @@ export async function parseQueryParams<Model> (collection: mongoose.Model<Model 
 	let page = Number.isNaN(Number(params.page)) ? 0 : Number(params.page)
 	page = page < 1 ? 1 : page
 
-	const total = await collection.countDocuments(whereClause).exec()
+	const total = await collection.countDocuments(totalClause).exec()
 
-	let builtQuery = collection.find(whereClause)
+	let builtQuery = collection.find(totalClause)
 	if (sortField) builtQuery = builtQuery.sort([[sortField, sortOrder]])
 	if (limit) builtQuery = builtQuery.limit(limit)
 	if (page && limit) builtQuery = builtQuery.skip((page - 1) * limit)
@@ -90,4 +76,28 @@ export type QueryResults<Model> = {
 		count: number
 	},
 	results: Model[]
+}
+
+const buildWhereQuery = (params: Where[], type: 'and' | 'or') => {
+	const where = params.map(({ field, value, condition }) => {
+			const checkedField = field === 'id' ? '_id' : (field ?? '')
+			const parsedValue = value ?? ''
+			const checkedValue = field === 'id'
+				? mongoose.Types.ObjectId.isValid(parsedValue) ? parsedValue : new mongoose.Types.ObjectId()
+				: (parsedValue ?? '')
+			const checkedCondition = Object.keys(Conditions).indexOf(condition as unknown as string) > -1 ? condition : Conditions.eq
+			return ({
+				field: checkedField,
+				value: checkedValue,
+				condition: checkedCondition
+			})
+		}).map((c) => ({
+			[`${ c.field }`]: {
+				[`$${ c.condition }`]: c.value
+			}
+		}))
+
+	return where.length > 0 ? {
+		[`$${type}`]: where
+	} : null
 }
