89d26f28e5016040c3ab267c553804f84b2052ea
kevinand11 <kevinfizu@gmail.com>
2021-09-23 18:50:41 +0100

    fix: send verfication emails

--- auth/src/utils/modules/emails.ts
+++ auth/src/utils/modules/emails.ts
@@ -1,13 +1,10 @@
-import { DeleteUsers, GetUsers, SendVerificationEmail } from '@modules/index'
+import { DeleteUsers, GetUsers } from '@modules/index'
 
 export const deleteUnverifiedUsers = async () => {
 	const unverifiedUsers = await getUnverifiedUsers()
 	const sevenDays = 7 * 24 * 60 * 60 * 1000
 	const olderUsers = unverifiedUsers.filter((u) => u.signedUpAt <= (Date.now() - sevenDays))
-	const recentUsers = unverifiedUsers.filter((u) => u.signedUpAt > (Date.now() - sevenDays))
-
 	await DeleteUsers.execute(olderUsers.map((u) => u.id))
-	await Promise.all(recentUsers.map(async (u) => await SendVerificationEmail.execute(u.email)))
 }
 
 const getUnverifiedUsers = async () => {
