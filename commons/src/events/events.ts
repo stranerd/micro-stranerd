949810efabff0f3845a347609627a81688d6c2ce
kevinand11 <kevinfizu@gmail.com>
2021-09-24 10:37:55 +0100

    fix: rabbit connection

--- commons/src/events/events.ts
+++ commons/src/events/events.ts
@@ -3,24 +3,23 @@ import { getRabbitConnection } from './rabbit'
 
 export class EventBus {
 	register = 'StranerdExchangeColumn'
+	conn = getRabbitConnection(this.register)
 
 	createPublisher<EventType extends EventTypes> (topic: EventType) {
-		const { register } = this
+		const { conn } = this
 
 		async function publish (data: Events[EventType]['data']) {
-			const conn = await getRabbitConnection(register)
-			await conn.publish(topic as unknown as string, JSON.stringify(data))
+			await (await conn).publish(topic as unknown as string, JSON.stringify(data))
 		}
 
 		return { publish }
 	}
 
 	createSubscriber<EventType extends EventTypes> (topic: EventType, onMessage: (data: Events[EventType]['data']) => void) {
-		const { register } = this
+		const { conn } = this
 
 		async function subscribe () {
-			const conn = await getRabbitConnection(register)
-			await conn.subscribe(topic as unknown as string, (data) => {
+			await (await conn).subscribe(topic as unknown as string, (data) => {
 				onMessage(JSON.parse(data))
 			})
 		}
