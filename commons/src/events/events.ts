605be7c6150a808bf2ab7b3b540f006eb5b5bb9c
kevinand11 <kevinfizu@gmail.com>
2021-09-24 11:42:30 +0100

    v1.0.5

--- commons/src/events/events.ts
+++ commons/src/events/events.ts
@@ -2,19 +2,23 @@ import { Events, EventTypes } from './eventTypes'
 import { pubAndSub } from './rabbit'
 
 export class EventBus {
+	ps = pubAndSub()
+
 	createPublisher<EventType extends EventTypes> (topic: EventType) {
+		const { ps } = this
+
 		async function publish (data: Events[EventType]['data']) {
-			const { publish } = await pubAndSub()
-			await publish(topic as unknown as string, JSON.stringify(data))
+			await (await ps).publish(topic as unknown as string, JSON.stringify(data))
 		}
 
 		return { publish }
 	}
 
 	createSubscriber<EventType extends EventTypes> (topic: EventType, onMessage: (data: Events[EventType]['data']) => void) {
+		const { ps } = this
+
 		async function subscribe () {
-			const { subscribe } = await pubAndSub()
-			await subscribe(topic as unknown as string, (data) => {
+			await (await ps).subscribe(topic as unknown as string, (data) => {
 				onMessage(JSON.parse(data))
 			})
 		}
