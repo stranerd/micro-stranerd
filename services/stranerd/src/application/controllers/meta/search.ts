17c64002d97a867f3283d569d49ad458d1951427
kevinand11 <kevinfizu@gmail.com>
2021-12-14 16:09:28 +0100

    feat: adding study to search

--- services/stranerd/src/application/controllers/meta/search.ts
+++ services/stranerd/src/application/controllers/meta/search.ts
@@ -1,30 +1,70 @@
 import { Request } from '@utils/commons'
 import { GetAnswers, GetQuestions } from '@modules/questions'
 import { GetUsers } from '@modules/users'
+import { GetFlashCards, GetNotes, GetSets, GetTestPreps, GetVideos } from '@modules/study'
 
 export class SearchController {
 	static async Search (req: Request) {
-		const questions = await GetQuestions.execute({
-			search: {
-				value: req.params.search,
-				fields: ['body']
-			},
-			limit: 10
-		})
-		const answers = await GetAnswers.execute({
-			search: {
-				value: req.params.search,
-				fields: ['title', 'body']
-			},
-			limit: 10
-		})
-		const users = await GetUsers.execute({
-			search: {
-				value: req.params.search,
-				fields: ['bio.firstName', 'bio.lastName']
-			},
-			limit: 10
-		})
-		return { questions, answers, users }
+		const [questions, answers, users, videos, notes, sets, flashCards, testPreps] = await Promise.all([
+			GetQuestions.execute({
+				search: {
+					value: req.params.search,
+					fields: ['body', 'tags']
+				},
+				limit: 10
+			}),
+			GetAnswers.execute({
+				search: {
+					value: req.params.search,
+					fields: ['title', 'body']
+				},
+				limit: 10
+			}),
+			GetUsers.execute({
+				search: {
+					value: req.params.search,
+					fields: ['bio.firstName', 'bio.lastName']
+				},
+				limit: 10
+			}),
+			GetVideos.execute({
+				search: {
+					value: req.params.search,
+					fields: ['title', 'description', 'tags']
+				},
+				limit: 10
+			}),
+			GetNotes.execute({
+				search: {
+					value: req.params.search,
+					fields: ['title', 'description', 'tags']
+				},
+				limit: 10
+			}),
+			GetSets.execute({
+				where: [{ field: 'isRoot', value: false }, { field: 'isPublic', value: true }],
+				search: {
+					value: req.params.search,
+					fields: ['name', 'tags']
+				},
+				limit: 10
+			}),
+			GetFlashCards.execute({
+				where: [{ field: 'isPublic', value: true }],
+				search: {
+					value: req.params.search,
+					fields: ['title', 'set', 'tags']
+				},
+				limit: 10
+			}),
+			GetTestPreps.execute({
+				search: {
+					value: req.params.search,
+					fields: ['name']
+				},
+				limit: 10
+			})
+		])
+		return { questions, answers, users, videos, notes, sets, flashCards, testPreps }
 	}
 }
