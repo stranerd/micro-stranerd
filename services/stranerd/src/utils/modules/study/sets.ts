d3f750b65aecb830ad6a30419a9b0b21b9eea70b
kevinand11 <kevinfizu@gmail.com>
2022-02-10 14:00:07 +0100

    fix: root set

--- services/stranerd/src/utils/modules/study/sets.ts
+++ services/stranerd/src/utils/modules/study/sets.ts
@@ -1,13 +1,26 @@
-import { GetSets, SetSaved, UpdateSetProp } from '@modules/study'
+import { AddSet, GetSets, SetSaved, UpdateSetProp } from '@modules/study'
+import { FindUser, UserEntity } from '@modules/users'
+
+export const createRootSet = async (user: UserEntity) => {
+	return await AddSet.execute({
+		name: '', parent: null, isPublic: false,
+		userId: user!.id, userBio: user!.bio, tags: []
+	})
+}
 
 export const getRootSet = async (userId) => {
-	const rootSet = (await GetSets.execute({
+	let rootSet = (await GetSets.execute({
 		where: [
 			{ field: 'parent', value: null },
 			{ field: 'userId', value: userId }
-		]
+		],
+		sort: { field: 'createdAt', order: 1 }
 	})).results[0]
-	return rootSet ?? null
+	if (!rootSet) {
+		const user = await FindUser.execute(userId)
+		rootSet = await createRootSet(user!)
+	}
+	return rootSet
 }
 
 export const saveNewItemToSet = async ({
@@ -15,7 +28,7 @@ export const saveNewItemToSet = async ({
 }: { itemId: string, userId: string, type: SetSaved, setId: string | null }) => {
 	if (!setId) {
 		const rootSet = await getRootSet(userId)
-		setId = rootSet?.id ?? null
+		setId = rootSet.id
 	}
 
 	if (setId) await UpdateSetProp.execute({
