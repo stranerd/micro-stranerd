297575d0d9a073ff2a8fc8a0dc4d7de8064736c4
kevinand11 <kevinfizu@gmail.com>
2022-05-03 09:57:03 +0100

    v3.8.2

--- services/api/src/utils/common/package/express/controllers/index.ts
+++ services/api/src/utils/common/package/express/controllers/index.ts
@@ -5,7 +5,8 @@ import { StorageFile } from '../../storage'
 
 type CustomResponse = {
 	status: SupportedStatusCodes,
-	result: any
+	result: any,
+	headers?: Record<string, any>
 }
 
 export type Controller = Handler | ErrorRequestHandler
@@ -13,7 +14,8 @@ export type Controller = Handler | ErrorRequestHandler
 export const makeController = (cb: (_: CustomRequest) => Promise<CustomResponse>): Controller => {
 	return async (req: Request, res: Response, next: NextFunction) => {
 		try {
-			const { status = StatusCodes.Ok, result } = await cb(extractRequest(req))
+			const { status = StatusCodes.Ok, result, headers = {} } = await cb(extractRequest(req))
+			Object.entries(headers).forEach(([key, value]) => res.header(key, value))
 			return res.status(status).json(result).end()
 		} catch (e) {
 			next(e)
@@ -35,7 +37,8 @@ export const makeMiddleware = (cb: (_: CustomRequest) => Promise<void>): Control
 
 export const makeErrorMiddleware = (cb: (_: CustomRequest, __: Error) => Promise<CustomResponse>): Controller => {
 	return async (err: Error, req: Request, res: Response, _: NextFunction) => {
-		const { status = StatusCodes.BadRequest, result } = await cb(extractRequest(req), err)
+		const { status = StatusCodes.BadRequest, result, headers = {} } = await cb(extractRequest(req), err)
+		Object.entries(headers).forEach(([key, value]) => res.header(key, value))
 		return res.status(status).json(result).end()
 	}
 }
