9e8ebe2bee8bd2247c7ca06b32c87b896a45b786
kevinand11 <kevinfizu@gmail.com>
2022-10-27 19:52:42 +0100

    fix: remove accesstoken requirement for google signin

--- services/api/src/application/controllers/auth/identities.ts
+++ services/api/src/application/controllers/auth/identities.ts
@@ -6,11 +6,9 @@ export class IdentitiesController {
 	static async googleSignIn (req: Request) {
 		const validatedData = validate({
 			idToken: req.body.idToken,
-			accessToken: req.body.accessToken,
 			referrer: req.body.referrer ?? null
 		}, {
 			idToken: { required: true, rules: [Validation.isString] },
-			accessToken: { required: true, rules: [Validation.isString] },
 			referrer: { required: true, nullable: true, rules: [Validation.isString] }
 		})
 
